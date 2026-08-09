5d8e39d238a19dc4353e27b7d7316316df2c92b8
AHallqvist <MY_NAME@example.com>
2026-08-09 21:00:17 +0200

    2.1 done

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+#print("hello world")
+
+def main():
+    textnode = TextNode("This is some anchor text", "link", url="https://www.boot.dev")
+    print(textnode)
+
+
+if __name__ == "__main__":
+    main()
+
